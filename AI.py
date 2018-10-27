d355663274e833bc52dc46777133d52ee87bb077
Aditya Acharya <aacha002@ucr.edu>
2018-10-27 15:59:41 -0700

    file setup

---
+++ AI.py
@@ -0,0 +1,35 @@
+
+
+
+
+def UniformCostSearch():
+
+    #arguments for pseudocode are problem, queueing-function
+    #nodese = make-queue(make-node(problem.InitialState))
+    #loop do
+    #   if EMPTY(nodes)
+    #   node = Remove-Front(nodes)
+    #   if problem.GOAL-StATE(node.STATE) succeeds then return node
+    #nodes = QUEUEingfunction(nodes, expand(node, problem.operator)
+
+
+
+
+
+
+def main():
+
+    puzzle = ([0,0,0], [0,0,0], [0,0,0])
+    #provide room for user input here
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
