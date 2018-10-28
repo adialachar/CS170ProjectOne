7dfd457a5565f6053719843005c1cb769c1d7c66
Aditya Acharya <aacha002@ucr.edu>
2018-10-27 19:18:08 -0700

    Started expand function

--- AI.py
+++ AI.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 
 
 def MisplacedTileCalculator(list):
@@ -38,6 +40,82 @@ def ManhattanDistanceCalculator(list):
 
 
 
+def InputFormatting(puzzle, a, b):
+
+
+    a = {'puzzle': puzzle, 'g(n)': int(a), 'h(n)': int(b)}
+
+
+    return a
+
+
+
+def expandManhattan(dict):
+
+    #find the 0
+    puzzle = dict['puzzle']
+
+    for i in range (0,3):
+        for j in range (0,3):
+            print(puzzle[i][j])
+            if (puzzle[i][j] == 0):
+                a = deepcopy(i)
+                b = deepcopy(j)
+
+
+    print("This should be a zero --> {}".format(puzzle[a][b]))
+    
+    up = False
+    down = False
+    left = False
+    right = False
+
+    if (a == 0):
+        down = True
+    elif (a == 1):
+        down = True
+        up = True
+    elif (a == 2):
+        up = True
+
+
+    if (b == 0):
+        right = True
+    elif (b == 1):
+        left = True
+        right = True
+    elif (b == 2):
+        left = True
+    
+    
+    print( "left {0}, right {1}, up {2}, down {3}".format(left, right, up, down))
+
+
+
+    if(left):
+        LeftChange = deepcopy(dict)
+        #swap we are using for now
+        temp = LeftChange['puzzle'][a-1][b]
+        LeftChange['puzzle'][a-1][b] = LeftChange['puzzle'][a][b]
+        LeftChange['puzzle'][a][b] = temp
+
+        LeftChange['g(n)'] += 1
+        LeftChange['h(n)'] = MannhattanDistanceCalculator(LeftChange['puzzle'])
+
+    if(right):
+        RightChange = deepcopy(dict)
+
+        #same as above
+    if(up):
+        UpChange = deepcopy(dict)
+
+    if(down):
+        DownChange = deepcopy(dict)
+
+
+
+
+
 
 
 
@@ -47,7 +125,7 @@ def UniformCostSearch():
     #arguments for pseudocode are problem, queueing-function
     #nodese = make-queue(make-node(problem.InitialState))
     #loop do
-    #   if EMPTY(nodes)
+    #   if EMPTY(nodes) then return failure
     #   node = Remove-Front(nodes)
     #   if problem.GOAL-StATE(node.STATE) succeeds then return node
     #nodes = QUEUEingfunction(nodes, expand(node, problem.operator)
@@ -62,16 +140,21 @@ def main():
     puzzle = list(([0,0,0], [0,0,0], [0,0,0]))
     puzzleOne = list(([1,2,3], [4,6,0], [7,5,8]))
     puzzleTwo = list(([1,2,0], [4,6,3], [7,5,8]))
-    a = ManhattanDistanceCalculator(puzzleOne)
-    b = ManhattanDistanceCalculator(puzzleTwo)
-    print("Manhattan")
-    print(a)
-    print(b)
-    c = MisplacedTileCalculator(puzzleOne)
-    d = MisplacedTileCalculator(puzzleTwo)
-    print("Misplaced Tiles")
-    print(c)
-    print(d)
+    #a = ManhattanDistanceCalculator(puzzleOne)
+    #b = ManhattanDistanceCalculator(puzzleTwo)
+    #print("Manhattan")
+    #print(a)
+    #print(b)
+    #c = MisplacedTileCalculator(puzzleOne)
+    #d = MisplacedTileCalculator(puzzleTwo)
+    #print("Misplaced Tiles")
+    #print(c)
+    #print(d)
+    e = InputFormatting(puzzleOne,5,6)
+    print(e['puzzle'])
+    print(e['g(n)'])
+    print(e['h(n)'])
+    expandManhattan(e)
     #provide room for user input here
 
 
