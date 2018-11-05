669b097f4f2aac1319cb24460ed2367a3ffbcad5
Aditya Acharya <aacha002@ucr.edu>
2018-11-05 05:20:22 -0800

    Fixed output, replaced recursion

--- AI.py
+++ AI.py
@@ -1,11 +1,7 @@
-from copy import deepcopy 
-import sys
+from copy import deepcopy
 
 solution = ([1,2,3], [4,5,6], [7,8,0]) #The solution to the 8-tile problem. 
 #If you want to change the goal state, change this array
-sys.setrecursionlimit(15000)
-
-
 
 def MisplacedTileCalculator(list): #Calculates and returns the number of misplaced tiles in a 2-D array
     
@@ -16,7 +12,7 @@ def MisplacedTileCalculator(list): #Calculates and returns the number of misplac
             if (solution[i][j] != list[i][j]):
                 MisplacedTiles += 1
 
-    return MisplacedTiles
+    return MisplacedTiles - 1
 
 
 
@@ -34,7 +30,7 @@ def ManhattanDistanceCalculator(list): #Calculates and returns the Manhattan dis
 
                     currVal = list[k][l]
 
-                    if (currVal == solVal):
+                    if (currVal == solVal and currVal != 0):
                         ManhattanDistance += (abs(i - k) + abs( j - l))
                         
 
@@ -147,20 +143,13 @@ def expand(dict, heuristicCalculator): #responsible for expanding the node passe
 
 
 
-
+nodesExpanded = 0
 
 
 
 def UniformCostCalculator(list):
 
-    #arguments for pseudocode are problem, queueing-function
-    #nodese = make-queue(make-node(problem.InitialState))
-    #loop do
-    #   if EMPTY(nodes) then return failure
-    #   node = Remove-Front(nodes)
-    #   if problem.GOAL-StATE(node.STATE) succeeds then return node
-    #nodes = QUEUEingfunction(nodes, expand(node, problem.operator)
-        
+
 
     return 0
 
@@ -170,22 +159,23 @@ def UniformCostCalculator(list):
 
     
 maxQlength = [] #keeps track of the max queue length of the input queue
-
+nodesExpanded = 0
 
 
 def ASTAR(list, heuristicCalculator, nodesExpanded):
 
+    k = 0
     
-        
-    if (len(list) == 0): #if EMPTY(nodes) then return failure
-        return False
+    while(k != len(list)):
+        if (len(list) == 0): #if EMPTY(nodes) then return failure
+            return False
 
-    maxQlength.append(len(list))
+        maxQlength.append(len(list))
 
     
-    minimum = 9999
-    minimumIndex = 0
-    for i in range (0, len(list)): #This set of loops finds the puzzle in the list with the lowest f(n), which wil become the node that is evaluated
+        minimum = 9999
+        minimumIndex = 0
+        for i in range (0, len(list)): #This set of loops finds the puzzle in the list with the lowest f(n), which wil become the node that is evaluated
             if ((list[i]['g(n)'] + list[i]['h(n)']) < minimum):
                 minimumIndex = deepcopy(i)
                 minimum = list[i]['g(n)'] + list[i]['h(n)']
@@ -195,41 +185,46 @@ def ASTAR(list, heuristicCalculator, nodesExpanded):
    
 
 
-    node = list.pop(minimumIndex) #node = RemoveFront(nodes) 
-    
-    print("Currently, the best node to expand is")
-    for i in range (0, len(node['puzzle'])):
+        node = list.pop(minimumIndex) #node = RemoveFront(nodes) 
+        print("The best state to expand with a g(n) = {0} and h(n) = {1} is...".format(node['g(n)'], node['h(n)']))
+        for i in range (0, len(node['puzzle'])):
+            print()
+            print("               ", end = '')
+            for j in range (0, len(node['puzzle'])):
+                if node['puzzle'][i][j] != 0:
+                    print(node['puzzle'][i][j] , end = ' ')
+                else:
+                    print('b', end = ' ')
+        print("  Expanding this node...")
         print()
-        for j in range (0, len(node['puzzle'])):
-            print(node['puzzle'][i][j] , end = ' ')
-    print()
-    print("With a g(n) of {} and a h(n) of {}".format(node['g(n)'], node['h(n)']))
-    print()
+        print()
     
     #print(node)
 
-
+        
     # these loops just check if the current state is the goal state
-    counter = 0
-    for i in range (0, 3):
-        for j in range (0,3):
-            if node['puzzle'][i][j] == solution[i][j]:
-                counter += 1
+        counter = 0
+        for i in range (0, 3):
+            for j in range (0,3):
+                if node['puzzle'][i][j] == solution[i][j]:
+                    counter += 1
 
-    if(counter == 9): #this verifires that each element in the list is the same as each element in the solution
-        print("Success!")
-        print("To solve this problem, the search algorithm expanded a total of {} nodes".format(nodesExpanded))
-        print("The maximum number of nodes in the queue at any one time was {}".format(max(maxQlength)))
-        print("The depth of the goal node was {}".format(node['g(n)']))
-        return node
+        if(counter == 9): #this verifires that each element in the list is the same as each element in the solution
+            print("Goal!")
+            print()
+            print("To solve this problem, the search algorithm expanded a total of {} nodes".format(nodesExpanded))
+            print("The maximum number of nodes in the queue at any one time was {}".format(max(maxQlength)))
+            print("The depth of the goal node was {}".format(node['g(n)']))
+            return node
 
 
 
-
-    mergedlist = list + expand(node,heuristicCalculator) #nodes = queueing-function(node, expand(node, problem.operator)
-    nodesExpanded += 1
-    
-    return ASTAR(mergedlist,heuristicCalculator, nodesExpanded) #recursive call to repeat process 
+        
+        list = list + expand(node,heuristicCalculator) #nodes = queueing-function(node, expand(node, problem.operator)
+        nodesExpanded += 1
+        
+        k += 1
+    #return ASTAR(mergedlist,heuristicCalculator, nodesExpanded) #recursive call to repeat process 
     
 
 
@@ -249,19 +244,17 @@ def main():
     userChoice = int(input())
 
     if(userChoice == 2):
-        print("Enter your puzzle use a zero to represent the blank")
-        print("Use commas to seperate the numbers")
-        print("Input should look like 1,2,3 (press enter) 4,5,6 (press enter) 7,8,0 (press enter)")
-        a = input()
-        b = input()
-        c = input()
+        print("     Enter your puzzle, use a zero to represent the blank")
+        a = input("     Enter the first row, use space or tabs between the numbers     ")
+        b = input("     Enter the second row, use space or tabs between the numbers    ")
+        c = input("     Enter the third row, use space or tabs between the numbers     ")
+    
         
         counter = 0
         for i in range (0, len(a)):
             if (a[i] != ' ' and a[i] != ','):
                 e['puzzle'][0][counter] = int(a[i])
-                print("These numbers should be equal {} <-----> {}".format(int(a[i]), e['puzzle'][0][counter]))
-                print("Counter is {}".format(counter))
+                
                 counter += 1
 
 
@@ -278,22 +271,32 @@ def main():
                 e['puzzle'][2][counter] = int(c[i])
                 counter += 1
 
-        #print(e['puzzle'])
+        
 
     Input = [e]
 
-
-    print("Enter your choice of algorithm")
-    print(" 1. Uniform Cost Search")
-    print(" 2. A* with the Misplaced Tile heruistic.")
-    print(" 3. A* with the Manhattan distance heuristic")
+    print()
+    print("     Enter your choice of algorithm")
+    print("     1. Uniform Cost Search")
+    print("     2. A* with the Misplaced Tile heruistic.")
+    print("     3. A* with the Manhattan distance heuristic")
+    print()
+    print("     ", end = '')
     userChoice = int(input())
+    print("Expanding state", end = '')
+    for l in range (0, len(e['puzzle'])):
+        if l > 0:
+                print("               ", end = '')
+        for m in range(0, len(e['puzzle'])):
+            print(e['puzzle'][l][m], end = ' ')
+        print()
+
     if userChoice == 1:
-        ASTAR(Input,UniformCostCalculator, 0) 
+        ASTAR(Input,UniformCostCalculator,0) 
     elif userChoice == 2:
-        ASTAR(Input, MisplacedTileCalculator, 0)
+        ASTAR(Input, MisplacedTileCalculator,0)
     elif userChoice == 3:
-        ASTAR(Input, ManhattanDistanceCalculator, 0)
+        ASTAR(Input, ManhattanDistanceCalculator,0)
     
     
 
